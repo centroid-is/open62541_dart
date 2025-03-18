826b1d15019691b927c5d34fc8a8448f99b40b9c
Jón Bjarni Bjarnason <jon@centroid.is>
2025-03-18 10:02:47 +0000

    now everything compiles on nixos

--- flake.nix
+++ flake.nix
@@ -24,7 +24,22 @@ outputs = { self, nixpkgs, flake-utils }:
             gtk3
             cmake
             gcc
+            llvmPackages_latest.libclang
+            clang
           ];
+
+          shellHook = ''
+            cp ${pkgs.writers.writeYAML "ffigen.yaml" {
+              llvm-path = [ "${pkgs.lib.getLib pkgs.llvmPackages_latest.libclang}/lib/libclang.so" ];
+              compiler-opts = [
+                "-isystem ${pkgs.lib.getLib pkgs.llvmPackages_latest.libclang}/lib/clang/${pkgs.lib.versions.major (pkgs.lib.getVersion pkgs.llvmPackages_latest.libclang)}/include"
+                "-idirafter ${pkgs.stdenv.cc.libc_dev}/include"
+              ];
+              headers.entry-points = [ "open62541_build/open62541.h" ];
+              output = "lib/bindings.dart";
+              name = "Open62541Bindings";
+            }} ffigen.yaml
+          '';
         };
     });
 }
