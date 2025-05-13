cba9fe174b1d98746a3dbdd578bddf3ee7a3a3a2
Ómar Högni Guðmarsson <omar@centroid.is>
2025-05-13 16:38:09 +0000

    Analyze failures

--- lib/open62541.dart
+++ lib/open62541.dart
@@ -5,4 +5,4 @@ export 'src/node_id.dart' show NodeId;
 export 'src/dynamic_value.dart' show DynamicValue, LocalizedText, EnumField;
 export 'src/library.dart' show Open62541Singleton;
 export 'src/generated/open62541_bindings.dart' show UA_STATUSCODE_GOOD;
-export 'src/extensions.dart' show UaTypes, MonitoringMode, AttributeId;
+export 'src/extensions.dart' show UaTypes, MonitoringMode, AttributeId, MessageSecurityMode;
