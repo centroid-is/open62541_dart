ea54f1fde22a4cd553ab36a1265f7f0317fafefb
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2025-05-17 15:10:32 +0000

    export exception type

--- lib/open62541.dart
+++ lib/open62541.dart
@@ -1,7 +1,7 @@
 library;
 
 export 'src/access_level.dart' show AccessLevelMask;
-export 'src/client.dart' show Client;
+export 'src/client.dart' show Client, Open62541ClientException;
 export 'src/dynamic_value.dart' show DynamicValue, LocalizedText, EnumField, Schema;
 export 'src/extensions.dart' show UaTypes, MonitoringMode, AttributeId, MessageSecurityMode, Namespace0Id;
 export 'src/generated/open62541_bindings.dart' show UA_STATUSCODE_GOOD;
