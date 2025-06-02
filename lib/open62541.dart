96295c29eeefa3515f5aaa2b2134d2d065ab5814
Jón Bjarni Bjarnason <jon@centroid.is>
2025-06-02 11:22:31 +0000

    expose log level

--- lib/open62541.dart
+++ lib/open62541.dart
@@ -4,7 +4,15 @@ export 'src/access_level.dart' show AccessLevelMask;
 export 'src/client.dart' show Client;
 export 'src/dynamic_value.dart' show DynamicValue, LocalizedText, EnumField, Schema;
 export 'src/extensions.dart'
-    show UaTypes, MonitoringMode, AttributeId, MessageSecurityMode, Namespace0Id, SecureChannelState, SessionState;
+    show
+        UaTypes,
+        MonitoringMode,
+        AttributeId,
+        MessageSecurityMode,
+        Namespace0Id,
+        SecureChannelState,
+        SessionState,
+        LogLevel;
 export 'src/generated/open62541_bindings.dart' show UA_STATUSCODE_GOOD;
 export 'src/node_id.dart' show NodeId;
 export 'src/server.dart' show Server;
