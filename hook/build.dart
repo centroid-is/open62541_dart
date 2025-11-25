f0d0c847fa1e228ba98e5af0252ed8de02a7e169
Ómar Högni Guðmarsson <omar@centroid.is>
2025-11-25 16:07:18 +0000

    Force windows to use the default generator

--- hook/build.dart
+++ hook/build.dart
@@ -50,6 +50,7 @@ Future<void> main(List<String> args) async {
       name: name,
       sourceDir: extractedFiles,
       buildMode: BuildMode.release,
+      generator: Platform.isWindows ? Generator.make : Generator.defaultGenerator,
       defines: {
         'CMAKE_BUILD_TYPE': 'Release',
         'CMAKE_INSTALL_PREFIX': '${input.outputDirectory.toFilePath()}/install',
