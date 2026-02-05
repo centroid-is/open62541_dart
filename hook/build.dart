1051967ced2fe248668ba1af05262b2857939264
Jón Bjarni Bjarnason <jon@centroid.is>
2026-02-05 07:24:45 +0000

    use mbedtls from build hooks

--- hook/build.dart
+++ hook/build.dart
@@ -7,6 +7,71 @@ import 'package:logging/logging.dart';
 import 'package:native_toolchain_cmake/native_toolchain_cmake.dart';
 import 'package:http/http.dart' as http;
 
+Future<Uri> _downloadMbedTLS(Uri baseDir) async {
+  final extractDir = Directory.fromUri(baseDir.resolve('mbedtls/download/'));
+  final url = 'https://github.com/Mbed-TLS/mbedtls/releases/download/mbedtls-3.6.5/mbedtls-3.6.5.tar.bz2';
+
+  final response = await http.get(Uri.parse(url));
+  if (response.statusCode != 200) {
+    throw Exception('Error downloading mbedTLS: ${response.statusCode}');
+  }
+
+  final decompressed = BZip2Decoder().decodeBytes(response.bodyBytes);
+  final archive = TarDecoder().decodeBytes(decompressed);
+
+  if (!await extractDir.exists()) {
+    await extractDir.create(recursive: true);
+  }
+
+  for (var file in archive) {
+    if (file.isFile) {
+      final outputStream = OutputFileStream('${extractDir.path.toString()}/${file.name}');
+      file.writeContent(outputStream);
+      outputStream.closeSync();
+    }
+  }
+  final folder = extractDir.listSync().firstWhere((element) => element is Directory).uri;
+  return folder;
+}
+
+Future<Uri> _buildMbedTLS(BuildInput input, BuildOutputBuilder output, Logger logger) async {
+  final mbedtlsBase = input.outputDirectoryShared.resolve('mbedtls/');
+  final installPrefix = mbedtlsBase.resolve('install/');
+
+  // Skip if already built
+  final includeCheck = File.fromUri(installPrefix.resolve('include/mbedtls/ssl.h'));
+  if (await includeCheck.exists()) {
+    logger.info('mbedTLS already built, skipping');
+    return installPrefix;
+  }
+
+  final sourceDir = await _downloadMbedTLS(input.outputDirectoryShared);
+
+  final builder = CMakeBuilder.create(
+    name: 'mbedtls',
+    sourceDir: sourceDir,
+    buildMode: BuildMode.release,
+    generator: Generator.defaultGenerator,
+    defines: {
+      'CMAKE_BUILD_TYPE': 'Release',
+      'CMAKE_INSTALL_PREFIX': installPrefix.toFilePath(),
+      'MBEDTLS_FATAL_WARNINGS': 'OFF',
+      'ENABLE_TESTING': 'OFF',
+      'ENABLE_PROGRAMS': 'OFF',
+      'USE_STATIC_MBEDTLS_LIBRARY': 'ON',
+      'BUILD_SHARED_LIBS': 'OFF',
+      'CMAKE_POSITION_INDEPENDENT_CODE': 'ON',
+    },
+    targets: ['install'],
+    buildLocal: true,
+    logger: logger,
+  );
+
+  await builder.run(input: input, output: output, logger: logger);
+
+  return installPrefix;
+}
+
 Future<Uri> download(Uri outputDirectory, String version) async {
   final extractDir = Directory.fromUri(outputDirectory.resolve('download/'));
 
@@ -49,6 +114,14 @@ Future<void> main(List<String> args) async {
       ..level = Level.ALL
       // temp fwd to stderr until process logs pass to stdout
       ..onRecord.listen((record) => stderr.writeln(record));
+
+    // Build mbedTLS first
+    final mbedtlsInstall = await _buildMbedTLS(input, output, logger);
+    final mbedtlsLibDir = mbedtlsInstall.resolve('lib/');
+    final isWindows = input.config.code.targetOS == OS.windows;
+    final libPrefix = isWindows ? '' : 'lib';
+    final libSuffix = isWindows ? '.lib' : '.a';
+
     final builder = CMakeBuilder.create(
       name: name,
       sourceDir: extractedFiles,
@@ -59,12 +132,16 @@ Future<void> main(List<String> args) async {
         'CMAKE_INSTALL_PREFIX': '${input.outputDirectory.toFilePath()}/install',
         'BUILD_SHARED_LIBS': 'ON',
         'UA_ENABLE_INLINABLE_EXPORT': 'ON',
-        'UA_ENABLE_ENCRYPTION': input.config.code.targetOS == OS.windows ? 'OPENSSL' : 'MBEDTLS',
+        'UA_ENABLE_ENCRYPTION': 'MBEDTLS',
         'UA_BUILD_EXAMPLES': 'OFF',
         'UA_BUILD_UNIT_TESTS': 'OFF',
         'UA_MULTITHREADING': '0',
         'UA_LOGLEVEL': '100',
         'UA_ENABLE_AMALGAMATION': 'ON',
+        'MBEDTLS_INCLUDE_DIRS': mbedtlsInstall.resolve('include/').toFilePath(),
+        'MBEDTLS_LIBRARY': mbedtlsLibDir.resolve('${libPrefix}mbedtls$libSuffix').toFilePath(),
+        'MBEDX509_LIBRARY': mbedtlsLibDir.resolve('${libPrefix}mbedx509$libSuffix').toFilePath(),
+        'MBEDCRYPTO_LIBRARY': mbedtlsLibDir.resolve('${libPrefix}mbedcrypto$libSuffix').toFilePath(),
       },
       targets: ['install'],
       buildLocal: true,
