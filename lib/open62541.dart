05cd1a87976f056c89214119314cc41e0d56e713
Jón Bjarni Bjarnason <jbbjarnason@gmail.com>
2026-03-03 17:53:10 +0000

    export SecureChannelClosed

--- lib/open62541.dart
+++ lib/open62541.dart
@@ -24,6 +24,6 @@ export 'src/third_party/open62541.g.dart'
         UA_OPEN62541_VERSION;
 export 'src/node_id.dart' show NodeId;
 export 'src/server.dart' show Server;
-export 'src/types/errors.dart' show Inactivity, SubscriptionDeleted;
+export 'src/types/errors.dart' show Inactivity, SecureChannelClosed, SubscriptionDeleted;
 export 'src/isolate.dart' show ClientIsolate;
 export 'src/client_api.dart' show ClientApi;
